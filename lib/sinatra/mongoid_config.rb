82c5873e0fca6b9ad7b83762433dc8c6a6770384
Matt Puchlerz <matt@puchlerz.com>
2010-06-19 23:17:19 -0500

    Added all the necessary files and gemspec.

---
+++ lib/sinatra/mongoid_config.rb
@@ -0,0 +1,47 @@
+require 'sinatra/base'
+require 'mongoid'
+
+
+
+module Mongoid
+  class Config
+    
+    # Sets a reference to the Sinatra app in which
+    # this extension is registered.
+    def sinatra_app= app
+      @sinatra_app = app unless @sinatra_app
+    end
+    
+    # If the database connection doesn't already exist,
+    # attempt to connect using options from the Sinatra app.
+    def master_with_autoconnect
+      unless @master
+        self.master = Mongo::Connection.new(@sinatra_app.mongo_host, @sinatra_app.mongo_port).db(@sinatra_app.mongo_db)
+        @master.authenticate(@sinatra_app.mongo_user, @sinatra_app.mongo_password) if @sinatra_app.mongo_user
+      end
+      master_without_autoconnect
+    end
+    
+    alias_method :master_without_autoconnect, :master
+    alias_method :master, :master_with_autoconnect
+    
+  end
+end
+
+
+
+module Sinatra
+  module MongoidConfig
+    
+    def self.registered app
+      Mongoid.configure.sinatra_app = app
+      
+      app.set :mongo_host,     ENV['MONGO_HOST'] || 'localhost'
+      app.set :mongo_db,       ENV['MONGO_DB']   || 'sinatra-mongoid'
+      app.set :mongo_port,     ENV['MONGO_PORT'] || Mongo::Connection::DEFAULT_PORT
+      app.set :mongo_user,     ENV['MONGO_USER']
+      app.set :mongo_password, ENV['MONGO_PASSWORD']
+    end
+    
+  end
+end
---
+++ sinatra-mongoid-config.gemspec
@@ -0,0 +1,17 @@
+Gem::Specification.new do |s|
+  s.name    = "sinatra-mongoid-config"
+  s.version = "0.0.1"
+
+  s.authors     = [ "Matt Puchlerz" ]
+  s.date        = "2010-06-19"
+  s.description = "Easy configuration when using Mongoid within a Sinatra app."
+  s.email       = "matt@puchlerz.com"
+  s.homepage    = "http://github.com/mattpuchlerz/sinatra-mongoid-config"
+  s.summary     = "Mongoid config for Sinatra."
+
+  s.files  = %w[ README.md ]
+  s.files += Dir["lib/**/*"]
+
+  s.add_dependency "sinatra", [ ">= 1.0" ]
+  s.add_dependency "mongoid", [ ">= 2.0.0" ]
+end
