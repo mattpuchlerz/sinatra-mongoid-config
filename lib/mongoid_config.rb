805f2a53114d1baccb612efe666d4a61f573fe25
Sascha Wessel <swessel@gr4yweb.de>
2011-11-27 19:12:45 +0100

    now the connection to mongo is really working.

--- lib/mongoid_config.rb
+++ lib/mongoid_config.rb
@@ -6,9 +6,10 @@ module Mongoid
     # If the database connection doesn't already exist,
     # attempt to connect using options from the Sinatra app.
     def master_with_autoconnect
+      @app = settings[:sinatra_app]
       unless @master
-        self.master = Mongo::Connection.new(@sinatra_app.mongo_host, @sinatra_app.mongo_port).db(@sinatra_app.mongo_db)
-        @master.authenticate(@sinatra_app.mongo_user, @sinatra_app.mongo_password) if @sinatra_app.mongo_user
+        self.master = Mongo::Connection.new(@app.mongo_host, @app.mongo_port).db(@app.mongo_db)
+        @master.authenticate(@app.mongo_user, @app.mongo_password) if @app.mongo_user
       end
       master_without_autoconnect
     end
